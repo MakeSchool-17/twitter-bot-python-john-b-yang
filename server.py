b5df6637303f3a3e0b7532901fc690e248103a09
John Yang <byjohnyang@gmail.com>
2017-10-08 18:31:40 -0700

    heroku commit

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ def hello_world():
     return make_sentence.run()
 
 if __name__ == '__main__':
+
     # Required for Heroku's port attachment during deployment
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
