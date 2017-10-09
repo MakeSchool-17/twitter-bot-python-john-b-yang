ea87fdfc912a85f5863f205c98c0c0eae2b03293
John Yang <byjohnyang@gmail.com>
2017-10-08 18:27:01 -0700

    LWW might have been too long, chose shorter book

--- generate_corpus.py
+++ generate_corpus.py
@@ -7,12 +7,17 @@ import sys, random, requests, re
 # Sample URL: http://api.diffbot.com/v3/article?token=yourtokenhere&url=https://en.wikipedia.org/wiki/Twitterbot
 DIFFBOT_API_URL = 'http://api.diffbot.com/v3/article'
 DIFFBOT_DEV_TOKEN = '658d3b5b3aa5add15a550691bb28064c'
-BOOK_URL = "https://archive.org/stream/TheChroniclesOfNarnia/The%20Chronicles%20of%20Narnia_djvu.txt"
+BOOK_URL = "https://archive.org/stream/twentythousandle00verniala/twentythousandle00verniala_djvu.txt"
 
 # Book URL List:
+# * Word Counts Here: http://commonplacebook.com/art/books/word-count-for-famous-novels/
 # 1. Cyrano de Bergerac: http://emotional-literacy-education.com/classic-books-online-b/cdben10.htm
 # 2. Wizard of Oz: http://www.gutenberg.org/files/55/55-h/55-h.htm
-# 3. Lion Witch & Wardrobe: https://archive.org/stream/TheChroniclesOfNarnia/The%20Chronicles%20of%20Narnia_djvu.txt
+# 3. Lion Witch & Wardrobe: https://archive.org/stream/TheChroniclesOfNarnia/The%20Chronicles%20of%20Narnia_djvu.txt (Works, book too long)
+# 4. Dr. Dolittle: http://www.gutenberg.org/files/501/501-h/501-h.htm (Not working)
+# 5. Alice in Wonderland: https://archive.org/stream/alicesadventures19033gut/19033.txt (Works okay, large blob of license text)
+# 6. Animal Farm: http://gutenberg.net.au/ebooks01/0100011h.html (Works, too short)
+# 7. Gulliver's Travels: http://www.gutenberg.org/files/829/829-h/829-h.htm
 
 def get_article(article_url):
     # Create request parameters for API call
@@ -42,3 +47,4 @@ if __name__ == '__main__':
     # Replace BOOK_URL with sys.argv[1] to take URL as parameter
     output_file_write.write(clean_article(get_article(BOOK_URL)))
     print('Corpus saved to {}'.format(output_file_write.name))
+    output_file_write.close()
--- server.py
+++ server.py
@@ -8,5 +8,6 @@ def hello_world():
     return make_sentence.run()
 
 if __name__ == '__main__':
+    # Required for Heroku's port attachment during deployment
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
