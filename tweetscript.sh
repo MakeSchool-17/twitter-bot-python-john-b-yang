48b0e6bf734aa6485090824e6fcd4a8b277a854c
John Yang <byjohnyang@gmail.com>
2017-10-18 14:28:38 -0700

    cronjob script added

--- tweetscript.sh
+++ tweetscript.sh
@@ -1,1 +1,1 @@
-python /Users/johnyang/Desktop/Classes/MakeSchool/TwitterBot/tweetjob.py
+python /Users/johnyang/Desktop/Classes/MakeSchool/TwitterBot/tweetjob.py | >> /Users/johnyang/Desktop/Classes/MakeSchool/TwitterBot/output.txt
