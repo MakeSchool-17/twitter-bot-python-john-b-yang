aca14e515530bd7aba3c07e441438f16e41825db
John Yang <byjohnyang@gmail.com>
2017-10-31 15:08:10 -0700

    modified script + modified gitignore

--- tweetjob.py
+++ tweetjob.py
@@ -1,4 +1,8 @@
-import make_sentence, twitter
+import make_sentence, twitter, datetime
 
 if __name__ == '__main__':
+    today = datetime.date.today()
+    print(today)
+    # TODO: Implement exponential randomness, make more days preferable than others
     twitter.tweet(make_sentence.run())
+    print("Completed Successfully")
--- tweetscript.sh
+++ tweetscript.sh
@@ -1,1 +1,3 @@
-python /Users/johnyang/Desktop/Classes/MakeSchool/TwitterBot/tweetjob.py | >> /Users/johnyang/Desktop/Classes/MakeSchool/TwitterBot/output.txt
+source activate py27
+python /Users/johnyang/Desktop/Classes/MakeSchool/TwitterBot/tweetjob.py >> /Users/johnyang/Desktop/Classes/MakeSchool/TwitterBot/output.txt
+source deactivate
